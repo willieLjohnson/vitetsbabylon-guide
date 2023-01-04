83dee885a837c1c3dbfb27b4cc66f67b9b13cab4
Willie Johnson <liwa.johnson@gmail.com>
2023-01-04 17:32:21 -0500

    update tutorial files

--- two-worldandplayer/devlon/src/game.ts
+++ two-worldandplayer/devlon/src/game.ts
@@ -3,9 +3,9 @@ import {
   Scene,
   Vector3,
   MeshBuilder,
-  FreeCamera,
   HemisphericLight,
-  KeyboardEventTypes,
+  UniversalCamera,
+  TransformNode,
 } from "babylonjs";
 import World from "./world/world";
 
@@ -13,14 +13,24 @@ export default class Game {
   engine: Engine;
   scene: Scene;
   world: World;
+  camera: UniversalCamera;
+  camRoot: TransformNode;
 
   constructor(readonly canvas: HTMLCanvasElement) {
     this.engine = new Engine(canvas);
     window.addEventListener("resize", () => {
       this.engine.resize();
     });
-    this.scene = createScene(this.engine, this.canvas);
+    this.scene = createScene(this.engine);
     this.world = new World(this.scene);
+    this.camera = new UniversalCamera(
+      "cam",
+      new Vector3(0, 20, -30),
+      this.scene
+    );
+    this.camRoot = new TransformNode("root");
+    this.setupCamera();
+    this.camera = this.registerCamera();
   }
 
   debug(debugOn: boolean = true) {
@@ -31,21 +41,58 @@ export default class Game {
     }
   }
 
+  public registerCamera(): UniversalCamera {
+    this.scene.registerBeforeRender(() => {
+      this.beforeRenderUpdate();
+      this.updateCamera();
+    });
+    return this.camera;
+  }
+
+  private beforeRenderUpdate(): void {
+    this.world.update();
+  }
+
+  private setupCamera(): UniversalCamera {
+    //root camera parent that handles positioning of the camera to follow the player
+    this.camRoot = new TransformNode("root");
+    this.camRoot.position = new Vector3(0, 0, 0); //initialized at (0,0,0)
+    //to face the player from behind (180 degrees)
+    this.camRoot.rotation = new Vector3(0, 0, 0);
+
+    //rotations along the x-axis (up/down tilting)
+    let yTilt = new TransformNode("ytilt");
+    //adjustments to camera view to point down at our player
+    yTilt.parent = this.camRoot;
+
+    //our actual camera that's pointing at our root's position
+    this.camera.lockedTarget = this.camRoot.position;
+    this.camera.fov = 0.47350045992678597;
+    this.camera.parent = yTilt;
+
+    this.scene.activeCamera = this.camera;
+    return this.camera;
+  }
+
+  private updateCamera(): void {
+    let player = this.world.player;
+    this.camRoot.position = Vector3.Lerp(
+      this.camRoot.position,
+      player.mesh.position,
+      0.4
+    );
+  }
+
   run() {
     this.debug(true);
     this.engine.runRenderLoop(() => {
       this.scene.render();
-      this.world.update();
     });
   }
 }
 
-var createScene = function (engine: Engine, canvas: HTMLCanvasElement) {
+var createScene = function (engine: Engine) {
   var scene = new Scene(engine);
-  var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-
-  camera.setTarget(Vector3.Zero());
-  camera.attachControl(canvas, true);
 
   var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
   light.intensity = 0.7;
